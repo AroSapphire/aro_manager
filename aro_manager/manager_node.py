d6403949c1121c5589b2ca5eac6622da9e2e8e94
AroSapphire <sali@arosystems.ai>
2025-08-03 20:07:12 +0500

    Added crash subscription topic for intentional crash

--- aro_manager/manager_node.py
+++ aro_manager/manager_node.py
@@ -2,21 +2,32 @@
 
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Empty
 
 class AroManager(Node):
     def __init__(self):
         super().__init__('aro_manager')
         self.get_logger().info('aro_manager is now running')
 
+        #Subscriber to crash topic
+        self.crash_sub = self.create_subscription(Empty, '/aro_manager/crash', self.crash_callback, 10)
+
+    
+    def crash_callback(self, msg):
+        self.get_logger().warn('Crash command received. Crashing aro_manager now...')
+        raise RuntimeError("Internal crash triggered via /aro_manager/crash")
+
 def main(args=None):
     rclpy.init(args=args)
     node = AroManager()
     try:
         rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    node.destroy_node()
-    rclpy.shutdown()
+    except Exception as e:
+        node.get_logger().error(f"Node crashed: {e}")
+        raise
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
