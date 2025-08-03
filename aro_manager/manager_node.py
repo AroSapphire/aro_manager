726c70a54c9ea171771aaaa11cc4cfc8e95ee2e6
AroSapphire <sali@arosystems.ai>
2025-08-03 20:12:37 +0500

    Added logging and log saving to ~/aro_logs

--- aro_manager/manager_node.py
+++ aro_manager/manager_node.py
@@ -3,11 +3,20 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Empty
+import logging
+import os
+from datetime import datetime
 
 class AroManager(Node):
     def __init__(self):
         super().__init__('aro_manager')
-        self.get_logger().info('aro_manager is now running')
+        self.logger = self._setup_file_logger()
+
+        # File log
+        self.logger.info('aro_manager is now running')
+        # Console log
+        self.get_logger().info('aro_manager node is now running')
+
 
         #Subscriber to crash topic
         self.crash_sub = self.create_subscription(Empty, '/aro_manager/crash', self.crash_callback, 10)
@@ -16,6 +25,30 @@ class AroManager(Node):
     def crash_callback(self, msg):
         self.get_logger().warn('Crash command received. Crashing aro_manager now...')
         raise RuntimeError("Internal crash triggered via /aro_manager/crash")
+    
+    def _setup_file_logger(self):
+        log_dir = os.path.expanduser('~/aro_logs')
+        os.makedirs(log_dir, exist_ok=True)
+
+        # Set log filename e,g aro_manager_20250803_193500.log
+        log_filename = os.path.join(
+            log_dir,
+            f"aro_manager_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+        )
+
+        logger = logging.getLogger('aro_manager_logger')
+        logger.setLevel(logging.DEBUG)
+
+        file_handler = logging.FileHandler(log_filename)
+        file_handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.propagate = False  # Prevent logs from showing up twice
+
+        return logger
 
 def main(args=None):
     rclpy.init(args=args)
@@ -23,9 +56,12 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except Exception as e:
+        node.logger.error(f"Node crashed: {e}")
         node.get_logger().error(f"Node crashed: {e}")
         raise
     finally:
+        node.logger.info("aro_manager is shutting down.")
+        node.get_logger().info("aro_manager is shutting down.")
         node.destroy_node()
         rclpy.shutdown()
 
